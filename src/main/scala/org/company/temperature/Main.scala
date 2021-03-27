34cfa286529b0c0ea9aa97ac32eae2172fb02bfa
AlexKitov <alkitov@gmail.com>
2021-03-28 00:22:52 +0100

    clean up

--- src/main/scala/org/company/temperature/Main.scala
+++ src/main/scala/org/company/temperature/Main.scala
@@ -29,6 +29,8 @@ object Main extends App {
 
   import spark.implicits._
 
+  spark.sparkContext.setLogLevel("ERROR")
+
   spark.conf.set("spark.sql.streaming.forceDeleteTempCheckpointLocation","True")
 
   var ds_vis: Dataset[Measurement] = spark.createDataset(Seq.empty[Measurement])
@@ -65,22 +67,12 @@ object Main extends App {
 
   println("Another solution below")
 
-//val spark:SparkSession = SparkSession.builder.master("local[3]").appName("SparkByExamples").getOrCreate()
-    spark.sparkContext.setLogLevel("ERROR")
-//  import spark.implicits._
-//  spark.conf.set("spark.sql.streaming.forceDeleteTempCheckpointLocation","True")
-
-//  val schema = StructType(List(StructField("line", StringType, true)))
-
-
-
   def mkString2(xml:Seq[String]): Seq[String] = {
       xml
         .mkString(" ")
         .split("<data>")
         .filter(_.nonEmpty)
         .map("<data>"+_)
-
   }
 
   val mkString2UDF=udf(mkString2 _)
@@ -89,33 +81,31 @@ object Main extends App {
     .option("maxFilesPerTrigger", 2)
     .textFile(dataPathString)
     .filter(line=> !line.startsWith(skipPattern))
-    .withColumn("rank",  lit(1))
-    .groupBy("rank")
+    .withColumn("timestamp", current_timestamp)
+    .withWatermark("timestamp", "1 minutes")
+    .groupBy("timestamp")
     .agg(collect_list("value").as("lst"))
-    .withColumn("hop", mkString2UDF(col("lst")))
-    .select(col("rank"), explode(col("hop")).as("tap"))
-    .select("tap")
-    .flatMap(r => parseXML(r.getString(0)))
+    .withColumn("lst_element", mkString2UDF(col("lst")))
+    .select(col("timestamp"), explode(col("lst_element")).as("element"))
+    .select("element")
+    .flatMap(r =>  parseXML(r.getString(0)))
+    .persist()
 
-  val windowSpec = Window.partitionBy(col("city")).orderBy(col("measured_at_ts") desc_nulls_last)
-
+//    val windowSpec = Window.partitionBy(col("city")).orderBy(col("measured_at_ts") desc_nulls_last)
 //    val df = consumer
-//    .withColumn("row_number", row_number over windowSpec)
-//    .filter("row_number == 1")
-//    .drop("row_number")
-//    .as[Measurement]
+//      .withColumn("row_number", row_number over windowSpec)
+//      .filter("row_number == 1")
+//      .drop("row_number")
+//      .as[MeasurementWithTimestamp]
 
 
   val producer = consumer.writeStream
     .format("console")
     .option("truncate", value = false)
     .option("numRows", 10)
-    .outputMode(Update) // <-- update output mode
-
+    .outputMode(Complete)   // <-- update output mode
 
   producer.start.awaitTermination()
-
-
 
   println( "TERMINATE!" )
 }
