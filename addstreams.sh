387c41fef662a91d9ac586a69950759117beb208
AlexKitov <alkitov@gmail.com>
2021-04-05 15:57:30 +0200

    add resources and stream manipulation scripts

---
+++ addstreams.sh
@@ -0,0 +1,4 @@
+hdfs dfs -copyFromLocal src/main/resources/stream1/* /input/stream1/
+hdfs dfs -copyFromLocal src/main/resources/stream2/* /input/stream2/
+hdfs dfs -copyFromLocal src/main/resources/stream3/* /input/stream3/
+hdfs dfs -copyFromLocal src/main/resources/city-country/population-stream.json /input/city-country/
---
+++ init.sh
@@ -0,0 +1,6 @@
+hdfs dfs -mkdir -p /input/stream1
+hdfs dfs -mkdir -p /input/stream2
+hdfs dfs -mkdir -p /input/stream3
+hdfs dfs -mkdir -p /input/city-country
+
+hdfs dfs -ls /
